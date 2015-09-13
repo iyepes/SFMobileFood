4d5f79f13ad141c9004b6c79d8be75854b81275f
Isabel Yepes <iyepes@hacemoscontactos.com>
2015-09-13 15:35:44 -0500

    Initial Commit

---
+++ SFMobileFoodTests/SFMobileFoodTests.swift
@@ -0,0 +1,36 @@
+//
+//  SFMobileFoodTests.swift
+//  SFMobileFoodTests
+//
+//  Created by Isabel Yepes on 13/09/15.
+//  Copyright (c) 2015 Hacemos Contactos. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SFMobileFoodTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
