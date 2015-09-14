bf429ba65d854642a336393951e39c6cd2b132d4
Isabel Yepes <iyepes@hacemoscontactos.com>
2015-09-14 02:00:47 -0500

    - Adding SODASample code

---
+++ SFMobileFood/QueryViewController.swift
@@ -0,0 +1,110 @@
+//
+//  QueryViewController.swift
+//  SODASample
+//
+//  Created by Frank A. Krueger on 8/10/14.
+//  Copyright (c) 2014 Socrata, Inc. All rights reserved.
+//
+
+import UIKit
+
+class QueryViewController: UITableViewController {
+    
+    // Register for access tokens here: http://dev.socrata.com/register
+    // CGxaHQoQlgQSev4zyUh5aR5J3
+    //let client = SODAClient(domain: "data.seattle.gov", token: "doOsHAaYFknfIi8v6gxUHVEzw")
+    
+    let client = SODAClient(domain: "data.sfgov.org", token: "doOsHAaYFknfIi8v6gxUHVEzw")
+    
+    let cellId = "DetailCell"
+    
+    var data: [[String: AnyObject]]! = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Create a pull-to-refresh control
+        refreshControl = UIRefreshControl ()
+        refreshControl?.addTarget(self, action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
+        
+        // Auto-refresh
+        refresh(self)
+    }
+    
+    /// Asynchronous performs the data query then updates the UI
+    func refresh (sender: AnyObject!) {
+        
+        //let cngQuery = client.queryDataset("3k2p-39jp").filter("within_circle(incident_location, 47.59815, -122.334540, 500) AND event_clearance_group IS NOT NULL")
+        
+        //https://data.sfgov.org/resource/rqzj-sfat.json
+        
+        let cngQuery = client.queryDataset("rqzj-sfat").filter("status = 'APPROVED'")
+        
+        //cngQuery.orderAscending("at_scene_time").get { res in
+        
+        cngQuery.orderAscending("expirationdate").get { res in
+            switch res {
+            case .Dataset (let data):
+                // Update our data
+                self.data = data
+            case .Error (let err):
+                let alert = UIAlertView(title: "Error Refreshing", message: err.userInfo.debugDescription, delegate: nil, cancelButtonTitle: "OK")
+                alert.show()
+            }
+            
+            // Update the UI
+            self.refreshControl?.endRefreshing()
+            self.tableView.reloadData()
+            self.updateMap(animated: true)
+        }
+    }
+    
+    /// Finds the map controller and updates its data
+    private func updateMap(#animated: Bool) {
+        if let tabs = (self.parentViewController?.parentViewController as? UITabBarController) {
+            if let mapNav = tabs.viewControllers![1] as? UINavigationController {
+                if let map = mapNav.viewControllers[0] as? MapViewController {
+                    map.updateWithData (data, animated: animated)
+                }
+            }
+        }
+    }
+    
+    override func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        // Show the map
+        if let tabs = (self.parentViewController?.parentViewController as? UITabBarController) {
+            tabs.selectedIndex = 1
+        }
+    }
+    
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let c = tableView.dequeueReusableCellWithIdentifier(cellId) as! UITableViewCell!
+        
+        let item = data[indexPath.row]
+        
+        //let name = item["event_clearance_description"]! as! String
+        let name = item["applicant"]! as! String
+        
+        let placeId = item["objectid"]! as! String
+        
+        var fullName = name + " " + placeId
+        
+        //c.textLabel?.text = name
+        c.textLabel?.text = fullName
+        /*
+        let street = item["hundred_block_location"]! as! String
+        let city = "Seattle"
+        let state = "WA"*/
+        
+        let street = item["address"]! as! String
+        let city = "San Francisco"
+        let state = "CA"
+        c.detailTextLabel?.text = "\(street), \(city), \(state)"
+        
+        return c
+    }
+}
