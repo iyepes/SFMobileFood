d467190ff8af7380d54e8d766d6fa64b99f46b83
Isabel Yepes <iyepes@hacemoscontactos.com>
2015-09-18 23:07:14 -0500

    - Framework issues solved

--- SODAKit/SODAKit.h
+++ SODAKit/SODAKit.h
@@ -2,18 +2,18 @@
 //  SODAKit.h
 //  SODAKit
 //
-//  Created by Frank A. Krueger on 8/9/14.
-//  Copyright (c) 2014 Socrata, Inc. All rights reserved.
+//  Created by Isabel Yepes on 18/09/15.
+//  Copyright (c) 2015 Hacemos Contactos. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for SocrataOpenData.
-FOUNDATION_EXPORT double SocrataOpenDataVersionNumber;
+//! Project version number for SODAKit.
+FOUNDATION_EXPORT double SODAKitVersionNumber;
 
-//! Project version string for SocrataOpenData.
-FOUNDATION_EXPORT const unsigned char SocrataOpenDataVersionString[];
+//! Project version string for SODAKit.
+FOUNDATION_EXPORT const unsigned char SODAKitVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SocrataOpenData/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <SODAKit/PublicHeader.h>
 
 
